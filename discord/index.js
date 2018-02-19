6a40cc42a63c0b101a1ed355a5b6b7ee20fa31b1
Protinam <protinam@protonmail.ch>
2018-02-18 23:23:21 -0800

    Log order cancellations

--- discord/index.js
+++ discord/index.js
@@ -1,27 +1,20 @@
-/* TODO:
- - Correct taker on order match
- - Link to asset page
- - Cancel order (event)
- * */
-
 const Discord = require('discord.js')
 const Web3 = require('web3')
 const BigNumber = require('bignumber.js')
 
 const { WyvernProtocol } = require('wyvern-js')
 const { WyvernExchange } = require('wyvern-exchange')
-var { tokens, schemas } = require('wyvern-schemas')
+var { tokens } = require('wyvern-schemas')
 
 const config = require('./config.json')
 const exchangeABI = require('./WyvernExchange.json')
 
 tokens = tokens[config.network]
 tokens = [].concat(tokens.canonicalWrappedEther, ...tokens.otherTokens)
-schemas = schemas[config.network]
 
 const wyvernExchange = new WyvernExchange(config.orderbook_url)
 const web3 = new Web3(config.web3_provider)
-const contract = new web3.eth.Contract(exchangeABI, config.exchange_address)
+const contract = new web3.eth.Contract(exchangeABI, WyvernProtocol.getExchangeContractAddress(config.network))
 const hook = new Discord.WebhookClient(config.webhook_id, config.webhook_token)
 
 const promisify = (inner) =>
@@ -46,29 +39,36 @@ const hookOnMatch = async (event) => {
   const buy = await fetchOrder(event.returnValues.buyHash)
   const sell = await fetchOrder(event.returnValues.sellHash)
   const order = buy || sell
-  const maker = order.maker
-  const taker = order.taker
+  const maker = event.returnValues.maker.toLowerCase()
+  const taker = event.returnValues.taker.toLowerCase()
   const token = tokens.filter(t => t.address.toLowerCase() === order.paymentToken.toLowerCase())[0]
   const amount = WyvernProtocol.toUnitAmount(new BigNumber(order.basePrice), token.decimals)
-  const schema = schemas.filter(s => s.name === order.metadata.schema)[0]
-  const title = schema.formatter(order.metadata.nft).title
-  hook.send(`**MATCH** - ${title} sold by [${maker}](${config.explorer_account_prefix + maker}) to [${taker}](${config.explorer_account_prefix + taker})\
+  hook.send(`**MATCH** - [${order.asset.formatted.title}](${'https://exchange.projectwyvern.com/assets/' + order.asset.hash}) sold by [${maker}](${config.explorer_account_prefix + maker}) to [${taker}](${config.explorer_account_prefix + taker})\
   for ${amount} ${token.symbol} - [Order](${'https://exchange.projectwyvern.com/orders/' + order.hash}) - [TX](${config.explorer_tx_prefix + event.transactionHash})`)
 }
 
 const hookOnPost = async (order) => {
-  const maker = order.maker
+  const maker = order.maker.toLowerCase()
   const token = tokens.filter(t => t.address.toLowerCase() === order.paymentToken.toLowerCase())[0]
   const amount = WyvernProtocol.toUnitAmount(new BigNumber(order.basePrice), token.decimals)
-  const schema = schemas.filter(s => s.name === order.metadata.schema)[0]
-  const title = schema.formatter(order.metadata.nft).title
   const which = order.side === 0 ? 'purchase' : 'sale'
-  hook.send(`**POST** - ${title} for ${which} by [${maker}](${config.explorer_account_prefix + maker}) for ${amount} ${token.symbol} - [Order](${'https://exchange.projectwyvern.com/orders/' + order.hash})`)
+  hook.send(`**POST** - [${order.asset.formatted.title}](${'https://exchange.projectwyvern.com/assets/' + order.asset.hash}) for ${which} by [${maker}](${config.explorer_account_prefix + maker}) for ${amount} ${token.symbol} - [Order](${'https://exchange.projectwyvern.com/orders/' + order.hash})`)
+}
+
+const hookOnCancel = async (event) => {
+  const order = await fetchOrder(event.returnValues.hash)
+  const maker = order.maker.toLowerCase()
+  const token = tokens.filter(t => t.address.toLowerCase() === order.paymentToken.toLowerCase())[0]
+  const amount = WyvernProtocol.toUnitAmount(new BigNumber(order.basePrice), token.decimals)
+  const which = order.side === 0 ? 'Purchase' : 'Sale'
+  hook.send(`**CANCEL** - ${which} of [${order.asset.formatted.title}](${'https://exchange.projectwyvern.com/assets/' + order.asset.hash}) for ${amount} ${token.symbol} cancelled by [${maker}](${config.explorer_account_prefix + maker}) - [Order](${'https://exchange.projectwyvern.com/orders/' + order.hash})`)
 }
 
 const handleEvents = async (block) => {
   const matchEvents = await promisify(c => contract.getPastEvents('OrdersMatched', {fromBlock: block, toBlock: block}, c))
   await Promise.all(matchEvents.map(hookOnMatch))
+  const cancelEvents = await promisify(c => contract.getPastEvents('OrderCancelled', {fromBlock: block, toBlock: block}, c))
+  await Promise.all(cancelEvents.map(hookOnCancel))
 }
 
 var first = false
@@ -76,7 +76,7 @@ var lastBlockNumber
 var hashes = []
 const poll = async () => {
   const blockNumber = await promisify(web3.eth.getBlockNumber)
-  const orders = await wyvernExchange.orders()
+  const orders = await wyvernExchange.orders({limit: 500})
   await Promise.all(orders.map(async order => {
     if (first && hashes.indexOf(order.hash) === -1) {
       return hookOnPost(order)
